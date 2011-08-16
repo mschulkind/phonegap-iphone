9afcc064f054cf3278f06005f4f9ca18c3c62a3d
Matthew Schulkind <mschulkind@gmail.com>
2011-08-16 01:01:58 -0400

    Rip out some pesky DLog()s.

--- PhoneGapLib/Classes/PluginResult.m
+++ PhoneGapLib/Classes/PluginResult.m
@@ -145,7 +145,6 @@ static NSArray* com_phonegap_CommandStatusMsgs;
 					[self.status intValue], self.message, [self.keepCallback boolValue]];
 	}
 
-	DLog(@"PluginResult:toJSONString - %@", resultString);
 	return resultString;
 }
 -(NSString*) toSuccessCallbackString: (NSString*) callbackId
@@ -158,7 +157,6 @@ static NSArray* com_phonegap_CommandStatusMsgs;
 	else {
 		successCB = [NSString stringWithFormat:@"PhoneGap.callbackSuccess('%@',%@);", callbackId, [self toJSONString]];			
 	}
-	DLog(@"PluginResult toSuccessCallbackString: %@", successCB);
 	return successCB;
 }
 -(NSString*) toErrorCallbackString: (NSString*) callbackId
@@ -171,7 +169,6 @@ static NSArray* com_phonegap_CommandStatusMsgs;
 	else {
 		errorCB = [NSString stringWithFormat:@"PhoneGap.callbackError('%@',%@);", callbackId, [self toJSONString]];
 	}
-	DLog(@"PluginResult toErrorCallbackString: %@", errorCB);
 	return errorCB;
 }	
 										 
